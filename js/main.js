7c1cba29ab110dff37885d509ef81d6a3d6d7e25
raphael <raphaelsantosdesigner@gmail.com>
2019-06-24 10:56:31 -0700

    close mobile menu

--- js/main.js
+++ js/main.js
@@ -2,31 +2,37 @@
 
  //OPEN MENU DESKTOP
  function openMenu() {
-     $('.full-menu').addClass('openMenu')
- }
+    $('.full-menu').addClass('openMenu')
+}
 
 function closeMenu() {
-    $('.full-menu').removeClass('openMenu')
+   $('.full-menu').removeClass('openMenu')
+}
+
+function closeMenuMobile() {
+   $('.full-menu-mobile').removeClass('openMenuMobile')
 }
 
 //OPEN MENU DESKTOP
 function openMenuMobile() {
-    $('.full-menu-mobile').addClass('openMenuMobile')
+   $('.full-menu-mobile').addClass('openMenuMobile')
 }
 
 
 $(document).ready(function () {
-    $('#viewMenu').on('click', function(){
-        openMenu();
-        openMenuMobile()
-    })
+   $('#viewMenu').on('click', function(){
+       openMenu();
+       openMenuMobile()
+   })
 
-    $('.full-menu').on('mouseleave',function(){
-        setTimeout(function(){ closeMenu(); 
-        }, 500);
-    })
+   $('.full-menu').on('mouseleave',function(){
+       setTimeout(function(){ closeMenu(); 
+       }, 500);
+   })
 
-    $(window).on('resize', function(){
-        heightMenuMobile()
-    });
+   $('.closeMenuMobile').on('click',function(){
+       setTimeout(function(){ closeMenuMobile(); 
+       }, 500);
+   })
+
 })
